bb09dfb2fefa5a63eb8ae22804a9d84601d9c8f7
vcersosimo93 <vcersosimo93@gmail.com>
2024-05-24 17:33:11 -0300

    arreglo del footer

--- src/components/fragments/footer/Footer.jsx
+++ src/components/fragments/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-green-900 w-screen h-12">
+    <footer className="bg-green-900 w-full h-12">
       <div className="flex flex-row justify-left items-center h-full">
         <span>
         <p className="text-white text-sm ml-3 mt-2">© 2024 Valora Consultora</p>
