9d554ece528a4b66bd9e61cd74eae9d77beec841
lrodriguez3161 <luciorodriguez3161@gmail.com>
2025-03-26 17:15:23 -0300

    All

--- src/api/ComparablesService.js
+++ src/api/ComparablesService.js
@@ -5,7 +5,8 @@ const API_URL = process.env.REACT_APP_API_URL;
 const ComparablesService = {
   getComparables: async (params) => {
     try {
-      const response = await axios.get(`https://api.mercadolibre.com/sites/MLU/search?category=MLU1459&OPERATION=242075&${params}&limit=30`);
+      const response = await axios.get(`${API_URL}/api/comparables?${params}&limit=30`);
+      // FIXME: BACKEND CAPAZ
       response.data.results.map((result) => {
         result.thumbnail = result.thumbnail.replace("I.jpg", "F.jpg");
         return result;
